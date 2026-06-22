62c89cee33de85d98cde0587bca2e707933a1ce2
andriibabiuk <andrii.babiuk.dev@gmail.com>
2026-06-22 16:25:12 +0300

    feat: add base routes for auth and messages

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
 const app = express();
 const PORT = 5000;
-app.get('/', (req, res) => {
-	res.send('Hello World');
-});
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
---
+++ backend/src/routes/message.route.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+const router = express.Router();
+
+router.get('/conversation', (req, res) => {
+	res.send('Conversation route');
+});
+export default router;
