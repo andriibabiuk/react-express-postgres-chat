8dbfe0b75139d886ca836bfa328417c6b7cf732b
andriibabiuk <andrii.babiuk.dev@gmail.com>
2026-06-23 11:27:37 +0300

    feat: add send message logic with conversation auto-creation

--- backend/src/routes/message.route.ts
+++ backend/src/routes/message.route.ts
@@ -1,7 +1,7 @@
 import express from 'express';
+import protectRoute from '../middleware/protectRoute.js';
+import { sendMessage } from '../controllers/message.controller.js';
 const router = express.Router();
 
-router.get('/conversation', (req, res) => {
-	res.send('Conversation route');
-});
+router.post('/send/:id', protectRoute, sendMessage);
 export default router;
