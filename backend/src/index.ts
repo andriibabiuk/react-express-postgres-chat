f86f3075ddb8b68116b745ca837db6e7a9100c8f
andriibabiuk <andrii.babiuk.dev@gmail.com>
2026-06-23 10:49:29 +0300

    feat: add protectRoute middleware and getMe endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import express from 'express';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 dotenv.config({ path: '../.env' });
 const app = express();
 const PORT = process.env.PORT;
+app.use(cookieParser());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
