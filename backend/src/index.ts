7612f166b0762bd2e100d353725a4870ab733205
andriibabiuk <andrii.babiuk.dev@gmail.com>
2026-06-24 16:24:16 +0300

    feat: implement useConversation zustand store

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 dotenv.config({ path: '../.env' });
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.use(cookieParser());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
