8b910176fd621684fbc33eaa251a20dd5add64d3
andriibabiuk <andrii.babiuk.dev@gmail.com>
2026-06-25 14:34:34 +0300

    feat: add newMessage socket event handling

--- frontend/src/components/messages/Messages.tsx
+++ frontend/src/components/messages/Messages.tsx
@@ -1,9 +1,11 @@
 import useGetMessages from '../../hooks/useGetMessages';
+import useListenMessages from '../../hooks/useListenMessages';
 import MessageSkeleton from '../skeletons/MessageSkeleton';
 import Message from './Message';
 
 const Messages = () => {
 	const { messages, loading } = useGetMessages();
+	useListenMessages();
 	return (
 		<div className='px-4 flex-1 overflow-auto'>
 			{loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
